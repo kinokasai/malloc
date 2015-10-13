ab81dd117f2f734910e08837cf6a19426f443fb7
Alexandre Doussot <dousso_a@pxe.pie.cri.epita.net>
2015-10-13 12:39:24 +0200

    Aligned memory on page size.

--- src/malloc.c
+++ src/malloc.c
@@ -1,13 +1,20 @@
 #define _GNU_SOURCE
 #include "malloc.h"
-#include <sys/mman.h>
 #include "errno.h"
 #include "debug.h"
+#include <sys/mman.h>
+#include <unistd.h>
+#include <limits.h>
 
 void *malloc(size_t size)
 {
     info("Trying to map %lu bytes of  memory", size);
-    void *mptr = mmap(NULL, 4096, PROT_READ | PROT_WRITE,
+    long page_size = sysconf(_SC_PAGESIZE);
+    unsigned long mps = page_size;
+    while (size >= mps)
+        mps += page_size;
+    info("Mapping %lu bytes of memory", mps);
+    void *mptr = mmap(NULL, mps, PROT_READ | PROT_WRITE,
                       MAP_ANONYMOUS | MAP_SHARED, -1, 0);
     if (mptr == MAP_FAILED)
     {
--- tests/main.c
+++ tests/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include "../src/debug.h"
 
 int main(void)
 {
     int *lol = malloc(sizeof (int));
+    void *erm = malloc(5123);
     *lol = 3;
     printf("lol: %d", *lol);
     return 0;
